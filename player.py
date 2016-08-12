990fb03766f51818eafb703dd6d16d69b93f2e72
ninhydrin <the.nocturne.of.daybreak@gmail.com>
2016-08-12 15:19:38 +0900

    add player class

---
+++ player.py
@@ -0,0 +1,12 @@
+import mj2
+
+class Player():
+
+    def __init__(self, seat):
+        self.seat = seat
+        self.point = 25000
+
+
+    def new_game(self, tehai, oya):
+        self.tehai = tehai
+        self.oya = oya
